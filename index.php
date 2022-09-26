d5109e5b65ffd3dcba4224a4811dba55563945aa
HilarioUA <josexd303@gmail.com>
2022-09-26 12:45:51 -0600

    index.php

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página principal</title>
+    <!-- Link para la hoja de estilos -->
+    <link rel="stylesheet" href="css/bootstrap/estilo.css">
+</head>
+<!-- Incluir el archido de cabecera -->
+<?php include('partes/cabecera.php'); ?>
+
+<body style="background-color: #ECECEC;">
+    <br><br><br><br><br>
+    <!-- Primer contenido de la página principal -->
+    <div class="container">
+        <center>
+            <h1>Un Pug</h1><br>
+        </center>
+        <div class="row"><br>
+            <p>
+                <img src="img/pug.jfif" align="left" hspace="20" width="300" height="300">
+                El perro (Canis familiaris o Canis lupus familiaris, dependiendo de si se lo considera una
+                especie por derecho propio o una subespecie del lobo),1​2​3​ llamado perro doméstico o can,4​ y
+                en algunos lugares coloquialmente llamado chucho,5​ tuso,6​ choco,7​ entre otros; es un mamífero
+                carnívoro de la familia de los cánidos, que constituye una especie del género Canis.8​9​ En el
+                2013, la población mundial estimada de perros estaba entre setecientos millones y novecientos
+                ochenta y siete millones.10​11​ Su tamaño (o talla), su forma y su pelaje es muy diverso y varía
+                según la raza. Posee un oído y un olfato muy desarrollados, y este último es su principal
+                órgano sensorial. Su longevidad media es de diez a trece años,12​13​14​ dependiendo de la raza.
+                Junto al gato doméstico, se le considera uno de los animales de compañía más populares del mundo.
+            </p>
+        </div>
+    </div>
+    <br><br>
+    <!-- Segundo contenido de la página principal -->
+    <div class="container">
+        <center>
+            <h1>Otra cosa</h1><br>
+            <img src="img/clima.jfif">
+            <br>
+            <p><br>
+                El clima se define como las condiciones meteorológicas medias que caracterizan a un lugar
+                determinado.1​2​ Es una síntesis del tiempo atmosférico, obtenida a partir de estadísticas a
+                largo plazo. Los elementos meteorológicos a tomar en cuenta son la temperatura, la presión,
+                el viento, la humedad y la precipitación.3​
+                El clima difiere del tiempo meteorológico en que la meteorología solo describe las condiciones
+                de corto plazo de estas variables en una región dada. Así, el clima de un lugar o una región
+                está constituido por los datos estadísticos de la meteorología de dicho lugar o región
+                analizados a lo largo de un plazo relativamente largo, de 30 años o más, como señala F. J.
+                Monkhouse (1978).4​5​ Los componentes meteorológicos que suelen tomarse como elementos
+                climáticos son cinco, como ya se ha indicado arriba.
+            </p>
+        </center>
+    </div>
+</body>
+
+</html>
