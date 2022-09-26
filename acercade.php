b973d0b66ec9a272c5c151eaec38fb72b613b865
HilarioUA <josexd303@gmail.com>
2022-09-26 12:45:41 -0600

    acercade.php

---
+++ acercade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acerca de:</title>
+</head>
+<?php include('partes/cabecera.php'); ?>
+<!-- Contenido -->
+<body>
+    <div>
+        <br><br><br><br><br><br><br>
+        <br><br><br><br><br>
+    </div>
+    <div class="container">
+        <center>
+            <h1>Acerca de Hola</h1>
+        </center>
+    </div>
+</body>
+
+</html>
